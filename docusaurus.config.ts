bf343165bfc1467835c11e884bfb67cf3e733f82
mariogkds <mariogk@blokods.com.br>
2025-01-13 17:51:01 +0000

    ✨ feat(ui): update navbar title to "Getting Started"
    Enhances user clarity by providing a more intuitive entry point.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,7 +69,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'Blater Documentation',
+      title: 'Getting Started',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
